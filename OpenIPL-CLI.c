612539dc45e86776bf2cf5e86d13eb8f49e775b9
LincolnCox29 <just.kcgo@gmail.com>
2025-04-12 19:40:11 +1000

    add -h flag

---
+++ OpenIPL-CLI.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+int main(int argc, char* argv[])
+{
+	if (argc < 2)
+	{
+		printf("%s\n%s",
+			   "[ERROR] Incorrect number of arguments",
+			   "Usage: OpenIPL-CLI <path> <command> [args...]");
+		return 1;
+	}
+	if (strcmp(argv[1], "-h") == 0)
+	{
+		printf("%s\n%s\n%s",
+			   "commands:",
+			   "	OpenIPL-CLI <path> <-bright> <factor>",
+			   "    e.g. OpenIPL-CLI C:\\Users\\User\\Desktop\\img.png -bright 0.5");
+	}
+}
